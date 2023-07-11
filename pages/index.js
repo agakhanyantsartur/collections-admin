678b26335f6fc582882254ab43fb43a4e34bbfd8
agakhanyantsartur <agakhanyantsartur@gmail.com>
2023-07-11 23:04:04 +0500

    Added styles and basic layout to dashboard and product sections (version-4)

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
-import { useSession, signIn, signOut } from "next-auth/react";
-import Nav from "../components/nav.js";
+import Layout from "@/components/layout";
+import {useSession} from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
-        </div>
+  const {data: session} = useSession();
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Hello, <b>{session?.user?.name}</b>;
+      </h2>
+      <div className="flex bg-gray-300 gap-1 text-blac rounded-lg overflow-hidden">
+        <img src={session?.user?.image} alt="photo" className="w-8 h-8"/>
+        <span className="px-2 py-0.5">
+          {session?.user?.name}
+        </span>
       </div>
-    )
-  }
-  return (
-    <div className="bg-blue-900 min-h-screen flex">
-      <Nav />
-      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">logged in {session.user.email}</div>
     </div>
-    
-  )
+  </Layout>
 }
