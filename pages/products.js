d7d36d14e96a9aa3f06da89ed14349f95a9ada77
agakhanyantsartur <agakhanyantsartur@gmail.com>
2023-07-15 23:44:03 +0500

    created the list of products added before (version-6)

--- pages/products.js
+++ pages/products.js
@@ -1,10 +1,36 @@
 import Layout from "@/components/Layout";
 import Link from "next/link";
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 export default function Products() {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    axios.get('/api/products').then(response => {
+      setProducts(response.data);
+    });
+  }, []);
   return (
     <Layout>
       <Link className="bg-blue-900 text-white rounded-md py-1 px-2" href={'/products/new'}>Add new product</Link>
+      <table>
+        <thead className="basic mt-2">
+          <tr>
+            <td>Product name</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(product => (
+            <tr>
+              <td>{product.title}</td>
+              <td>
+                buttons
+              </td>
+            </tr>
+          ))}
+        </tbody>
+    </table>
     </Layout>
   );
 }
