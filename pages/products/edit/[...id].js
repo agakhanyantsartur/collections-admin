f26386529df75d3af8005d18c2ce376ed5fa0fd6
agakhanyantsartur <agakhanyantsartur@gmail.com>
2023-07-17 19:18:37 +0500

    Modified products page and added some styling (version-66 upd)

---
+++ pages/products/edit/[...id].js
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import { useRouter } from "next/router";
+
+export default function EditProductPage() {
+  const router = useRouter();
+  
+  return (
+    <Layout>
+      edit product form here
+    </Layout>
+  )
+}
